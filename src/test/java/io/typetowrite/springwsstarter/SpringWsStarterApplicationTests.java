214564b7db4bfa53cabceaa7ea86bcaa5707a286
Tiffany Sonny <tiffanysonny@gmail.com>
2023-01-07 12:19:35 -0800

    initial commit

---
+++ src/test/java/io/typetowrite/springwsstarter/SpringWsStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package io.typetowrite.springwsstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWsStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
