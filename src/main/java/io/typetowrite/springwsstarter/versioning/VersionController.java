3d642898859b47303ffe4844e1a5c1f84a77dcc9
Tiffany Sonny <tiffanysonny@gmail.com>
2023-01-08 01:44:16 -0800

    added other version examples

--- src/main/java/io/typetowrite/springwsstarter/versioning/VersionController.java
+++ src/main/java/io/typetowrite/springwsstarter/versioning/VersionController.java
@@ -13,4 +13,31 @@ public class VersionController {
     public UserV2 getSecondVersionUser(){
         return new UserV2("Dolly", "Parton");
     }
+
+    @GetMapping(path="/user", params = "version=1")
+    public UserV1 getFirstVersionPath(){
+        return new UserV1("Betty Ruble");
+    }
+    @GetMapping(path =  "/user", params = "version=2")
+    public UserV2 getSecondVersionPath(){
+        return new UserV2("Betty", "Ruble");
+    }
+
+    @GetMapping(path = "/user/header", headers = "X-API-VERSION=1")
+    public UserV1 getFirstVersionHeader() {
+        return new UserV1("Holly GoLightly");
+    }
+
+    @GetMapping(path = "/user/header", headers = "X-API-VERSION=2")
+    public UserV2 getSecondVersionHeader(){
+        return new UserV2("Holly", "GoLightly");
+    }
+    @GetMapping(value = "/user/accept", produces = "application/io.typetowrite.app-v1+json")
+    public UserV1 getFirstVersionAccept(){
+        return new UserV1("Taylor Swift");
+    }
+    @GetMapping(value = "/user/accept", produces = "application/io.typetowrite.app-v2+json")
+    public UserV2 getSecondVersionAccept(){
+        return new UserV2("Taylor", "Swift");
+    }
 }
