4afa141beab4490724050bde2d3c9f622faa5c0b
Tiffany Sonny <tiffanysonny@gmail.com>
2023-01-08 21:46:45 -0800

    configured api specific security options

---
+++ src/main/java/io/typetowrite/springwsstarter/security/SpringSecurityConfig.java
@@ -0,0 +1,21 @@
+package io.typetowrite.springwsstarter.security;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpringSecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(
+                auth -> auth.anyRequest().authenticated()
+        );
+        http.httpBasic(withDefaults());
+        http.csrf().disable();
+        return http.build();
+    }
+}
